56aaf1391ac4aa881e7a3338c86994ad7c4c0b32
IceOPede <burgherrp@bzz.ch>
2018-01-18 14:31:54 +0100

    file upload

--- src/main/java/vidshare/demo/App.java
+++ src/main/java/vidshare/demo/App.java
@@ -1,10 +1,14 @@
 package vidshare.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import test.VideoCounter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,4 +23,16 @@ public class App {
         VideoCounter videoCounter = new VideoCounter();
         return videoCounter.getVideoPathList();
     }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ModelAndView upload(@RequestParam("file") MultipartFile file) throws IOException {
+
+        VideoCounter videoCounter = new VideoCounter();
+        File cFile = new File("C:\\_ws\\VidShare\\src\\main\\resources\\static\\Videos\\"+ (videoCounter.getVideosCount()+1)+".mp4");
+        cFile.createNewFile();
+        FileOutputStream out = new FileOutputStream(cFile);
+        out.write(file.getBytes());
+        return new ModelAndView("redirect:Index.html");
+    }
+
 }
--- src/test/java/vidshare/demo/DemoApplicationTests.java
+++ src/test/java/vidshare/demo/DemoApplicationTests.java
@@ -2,10 +2,8 @@ package vidshare.demo;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import test.VideoCounter;
 
-@SpringBootTest
 public class DemoApplicationTests {
 
 	@Test
